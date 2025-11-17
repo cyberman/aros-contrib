e10918b32135f9f508f01d186752437da804a17c
Johan Gill <johan.gill@gmail.com>
2025-11-17 01:21:21 +0100

    Address 4 is not guaranteed to hold a pointer to SysBase. Also simplify some library management code and rely on the globals.

--- regina/rexxmast/RexxMast.c
+++ regina/rexxmast/RexxMast.c
@@ -85,14 +85,6 @@ void server_process(void)
       "OK"
    };
 
-   struct Library *DebugBase = NULL, *DOSBase = NULL, *IntuitionBase = NULL;
-   struct Library *SysBase = *(struct Library **)4;
-   DebugBase = OpenLibrary("debug.library", 0);
-   if (!DebugBase) goto cleanup;
-   DOSBase = OpenLibrary("dos.library", 0);
-   if (!DOSBase) goto cleanup;
-   IntuitionBase = OpenLibrary("intuition.library", 0);
-   if (!IntuitionBase) goto cleanup;
    lock = Lock("PROGDIR:", SHARED_LOCK);
    NameFromLock(lock, progdir, sizeof(progdir));
    D(bug("Got PROGDIR:='%s'\n", progdir));
@@ -212,12 +204,6 @@ void server_process(void)
    cleanup:
    if (port)
       DeletePort(port);
-   if (DOSBase)
-      CloseLibrary(DOSBase);
-   if (IntuitionBase)
-      CloseLibrary(IntuitionBase);
-   if (DebugBase)
-      CloseLibrary(DebugBase);
 }
 
 static LONG StartFile(struct RexxMsg *msg, UBYTE *progdir)
